0eefa00f486b3582fed8da4ddbd7405e05ae4eb0
Daniel Ortiz <danielfelipeortizbonilla@outlook.com>
2020-03-17 10:58:31 -0500

    Panel Juego - Controlador
    Actualizacipon de Panel Juego y de Controllador, con el metodo Verificar X

--- MariaCasquitosTICTACTOE/src/co/edu/unbosque/controller/Controller.java
+++ MariaCasquitosTICTACTOE/src/co/edu/unbosque/controller/Controller.java
@@ -19,7 +19,44 @@ public class Controller implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-
+	public void actionPerformed(ActionEvent e) {
+		
+		int contadorMovimientos = 0;
+		if (e.getSource().equals(gui.getPanelBoton().getBotonJugar())) {
+			
+			
+			contadorMovimientos++;
+			System.out.println(contadorMovimientos);
+			
+		}
+		
 	}
+	
+	public void asignarValoresMatrizJuego(){
+		
+	}
+
+	/**public String verificarX( )
+			throws ValorDiferenteDeXExcepcion {
+		try {
+			if (textoJTextFile != 'X' || textoJTextFile != 'x') {
+				throw new ValorDiferenteDeXExcepcion();
+			} else if (textoJTextFile == 'x') {
+				convertirX(posFila, posColumna);
+			} else if (textoJTextFile == 'X') {
+				juego.asignarValorMatriz(posFila, posColumna, textoJTextFile);
+				
+			}
+		} catch (ValorDiferenteDeXExcepcion e) {
+			return e.getMessage();
+		}
+		return "Ok";
+	}
+
+	public void convertirX(int posFila, int posColumna) {
+		char converX = juego.mostrarValorMatriz(posFila, posColumna);
+		converX = Character.toUpperCase(converX);
+		juego.asignarValorMatriz(posFila, posColumna, converX);
+	}**/
+	
 }
--- MariaCasquitosTICTACTOE/src/co/edu/unbosque/view/PanelJuego.java
+++ MariaCasquitosTICTACTOE/src/co/edu/unbosque/view/PanelJuego.java
@@ -11,7 +11,15 @@ public class PanelJuego extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	private JTextField matrizJuego[][];
+	private JTextField posicion1;
+	private JTextField posicion2;
+	private JTextField posicion3;
+	private JTextField posicion4;
+	private JTextField posicion5;
+	private JTextField posicion6;
+	private JTextField posicion7;
+	private JTextField posicion8;
+	private JTextField posicion9;
 
 	/**
 	 * Metodo constructor del panel de juego.
@@ -32,37 +40,127 @@ public class PanelJuego extends JPanel {
 	 * 
 	 */
 	private void llenarPanel() {
+		posicion1 = new JTextField();
+		posicion1.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		// el
+		// texto
+		// en
+		// el
+		// centro
+		posicion1.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		// el
+		// tipo
+		// y
+		// tamaño
+		// de
+		// letra
+		add(posicion1); // Se añaden los campos de textos a la
+		// matriz
+		posicion2 = new JTextField();
+		posicion2.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion2.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion2);
+		posicion3 = new JTextField();
+		posicion3.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion3.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion3);
+		posicion4 = new JTextField();
+		posicion4.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion4.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion4);
+		posicion5 = new JTextField();
+		posicion5.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion5.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion5);
+		posicion6 = new JTextField();
+		posicion6.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion6.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion6);
+		posicion7 = new JTextField();
+		posicion7.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion7.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion7);
+		posicion8 = new JTextField();
+		posicion8.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion8.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion8);
+		posicion9 = new JTextField();
+		posicion9.setHorizontalAlignment(JLabel.CENTER); // Alinear
+		posicion9.setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
+		add(posicion9);
 
-		matrizJuego = new JTextField[3][3];
-		// cargar
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				matrizJuego[i][j] = new JTextField();
-				matrizJuego[i][j].setHorizontalAlignment(JLabel.CENTER); // Alinear
-																			// el
-																			// texto
-																			// en
-																			// el
-																			// centro
-				matrizJuego[i][j].setFont(new java.awt.Font("Tahoma", 1, 90)); // Cambia
-																				// el
-																				// tipo
-																				// y
-																				// tamaño
-																				// de
-																				// letra
-				add(matrizJuego[i][j]); // Se añaden los campos de textos a la
-										// matriz
-			}
-		}
 	}
 
-	public JTextField[][] getMatrizJuego() {
-		return matrizJuego;
+	public JTextField getPosicion1() {
+		return posicion1;
 	}
 
-	public void setMatrizJuego(JTextField[][] matrizJuego) {
-		this.matrizJuego = matrizJuego;
+	public void setPosicion1(JTextField posicion1) {
+		this.posicion1 = posicion1;
+	}
+
+	public JTextField getPosicion2() {
+		return posicion2;
+	}
+
+	public void setPosicion2(JTextField posicion2) {
+		this.posicion2 = posicion2;
+	}
+
+	public JTextField getPosicion3() {
+		return posicion3;
+	}
+
+	public void setPosicion3(JTextField posicion3) {
+		this.posicion3 = posicion3;
+	}
+
+	public JTextField getPosicion4() {
+		return posicion4;
+	}
+
+	public void setPosicion4(JTextField posicion4) {
+		this.posicion4 = posicion4;
+	}
+
+	public JTextField getPosicion5() {
+		return posicion5;
+	}
+
+	public void setPosicion5(JTextField posicion5) {
+		this.posicion5 = posicion5;
+	}
+
+	public JTextField getPosicion6() {
+		return posicion6;
+	}
+
+	public void setPosicion6(JTextField posicion6) {
+		this.posicion6 = posicion6;
+	}
+
+	public JTextField getPosicion7() {
+		return posicion7;
+	}
+
+	public void setPosicion7(JTextField posicion7) {
+		this.posicion7 = posicion7;
+	}
+
+	public JTextField getPosicion8() {
+		return posicion8;
+	}
+
+	public void setPosicion8(JTextField posicion8) {
+		this.posicion8 = posicion8;
+	}
+
+	public JTextField getPosicion9() {
+		return posicion9;
+	}
+
+	public void setPosicion9(JTextField posicion9) {
+		this.posicion9 = posicion9;
 	}
 
 }
