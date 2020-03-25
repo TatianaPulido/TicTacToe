f8f4cd899bca0acf98e318cc1df16761d395e3b7
Tatiana Pulido <Tatiana Pulido@192.168.0.5>
2020-03-24 20:01:48 -0500

    Movimientos PC

--- CSETICTACTOE/src/co/edu/unbosque/controller/Controller.java
+++ CSETICTACTOE/src/co/edu/unbosque/controller/Controller.java
@@ -2,27 +2,22 @@ package co.edu.unbosque.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.font.NumericShaper;
-
-import javax.swing.JOptionPane;
 
 import co.edu.unbosque.model.Juego;
 import co.edu.unbosque.view.View;
 
 public class Controller implements ActionListener {
-	
+
 	private Juego juego;
 	private View gui;
 	private int contadorClick;
-	
+
 	public Controller() {
 		juego = new Juego();
 		gui = new View(this);
 		contadorClick = 0;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent a) {
 
@@ -33,68 +28,84 @@ public class Controller implements ActionListener {
 			} catch (ValorDiferenteDeXExcepcion | SoloUnTurnoALaVezExcepcion e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	public void jugar() throws ValorDiferenteDeXExcepcion,
-	SoloUnTurnoALaVezExcepcion {
+			SoloUnTurnoALaVezExcepcion {
 		contadorClick++;
-		
+
 		gui.getPanelJuego().cargarValoresJTextField();
-		
+
 		String valorPosiciones = "";
 		int contadorCasillasOcupadas = 0;
-		
+
 		for (int i = 0; i < 9; i++) {
 			valorPosiciones = gui.getPanelJuego().mostrarValoresJTextField()[i];
-			
+
 			if (valorPosiciones.length() != 0) {
-				contadorCasillasOcupadas++;	
-			}
-			
-		}
-		if (contadorCasillasOcupadas == contadorClick ) {
-			for (int j = 0; j< 9; j++) {
-				valorPosiciones = gui.getPanelJuego()
-						.mostrarValoresJTextField()[j];
-				verificarX(valorPosiciones, j);
+				contadorCasillasOcupadas++;
 			}
 
+		}
+		if (contadorCasillasOcupadas == (contadorClick + juego.numerojugadasPC)) {
+			if (contadorClick == 1) {
+				for (int j = 0; j < 9; j++) {
+					valorPosiciones = gui.getPanelJuego()
+							.mostrarValoresJTextField()[j];
+					verificarX(valorPosiciones, j);
+				}
+			} else {
+				for (int k = 0; k < 9; k++) {
+					if (k != juego.posicionJugadaPC[0]
+							&& k != juego.posicionJugadaPC[1]
+							&& k != juego.posicionJugadaPC[2]
+							&& k != juego.posicionJugadaPC[3]) {
+
+						valorPosiciones = gui.getPanelJuego()
+								.mostrarValoresJTextField()[k];
+						verificarX(valorPosiciones, k);
+
+					}
+				}
+			}
+			int pos = juego.jugarPC();
+			gui.getPanelJuego().asignarValorJTextField("O", pos);
+			gui.getPanelJuego().inhabilitarCampos(pos);
 		} else {
 
 			soloUnValor();
 
-
 		}
 	}
-	
-	public void soloUnValor(){
-		
+
+	public void soloUnValor() {
+
 		try {
-			
+
 			contadorClick--;
 			throw new SoloUnTurnoALaVezExcepcion();
-			
+
 		} catch (SoloUnTurnoALaVezExcepcion e) {
-			
+
 			gui.mostrarResultados(e.getMessage());
-		
+
 		}
-		
+
 	}
-	
+
 	public void verificarX(String textoJTextFile, int posicion)
 			throws ValorDiferenteDeXExcepcion {
-		
+
 		try {
-			
+
 			if (textoJTextFile.equals("x")) {
 				convertirX(textoJTextFile, posicion);
 				gui.getPanelJuego().inhabilitarCampos(posicion);
 			} else if (textoJTextFile.equals("X")) {
-				juego.asignarValorMatriz(1, posicion);
+				juego.asignarValorMatriz(2, posicion);
 				gui.getPanelJuego().inhabilitarCampos(posicion);
 			} else if (textoJTextFile.length() == 0) {
 				juego.asignarValorMatriz(0, posicion);
@@ -103,20 +114,20 @@ public class Controller implements ActionListener {
 				contadorClick--;
 				throw new ValorDiferenteDeXExcepcion();
 			}
-			
+
 		} catch (ValorDiferenteDeXExcepcion e) {
-			
+
 			gui.mostrarResultados(e.getMessage());
-			
+
 		}
-		
+
 	}
-	
+
 	public void convertirX(String textoJTextFile, int posicion) {
-		
+
 		String converX = textoJTextFile.toUpperCase();
 		gui.getPanelJuego().asignarValorJTextField(converX, posicion);
-		juego.asignarValorMatriz(1, posicion);
+		juego.asignarValorMatriz(2, posicion);
 	}
-	
+
 }
--- CSETICTACTOE/src/co/edu/unbosque/model/Juego.java
+++ CSETICTACTOE/src/co/edu/unbosque/model/Juego.java
@@ -3,28 +3,43 @@ package co.edu.unbosque.model;
 import javax.swing.JOptionPane;
 
 public class Juego {
-	
+
 	private int matrizJuego[][];
-	
+	public int posicionJugadaPC[];
+	public int numerojugadasPC;
+	private boolean yaSeJugo;
+
 	public Juego() {
-		
+
 		matrizJuego = new int[3][3];
+		posicionJugadaPC = new int[4];
+		yaSeJugo = false;
 		inicializarMatriz();
-		
+		inicializarArreglo();
 	}
-	
+
+	private void inicializarArreglo() {
+
+		for (int i = 0; i < 4; i++) {
+
+			posicionJugadaPC[i] = -1;
+
+		}
+
+	}
+
 	private void inicializarMatriz() {
-		
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				matrizJuego[i][j] = 0;
 			}
 		}
-		
+
 	}
-	
+
 	public void asignarValorMatriz(int valorDeJugada, int posicion) {
-		
+
 		if (posicion == 0) {
 			matrizJuego[0][0] = valorDeJugada;
 		} else if (posicion == 1) {
@@ -35,33 +50,88 @@ public class Juego {
 			matrizJuego[1][0] = valorDeJugada;
 		} else if (posicion == 4) {
 			matrizJuego[1][1] = valorDeJugada;
-		} else if (posicion == 6) {
+		} else if (posicion == 5) {
 			matrizJuego[1][2] = valorDeJugada;
-		} else if (posicion == 7) {
+		} else if (posicion == 6) {
 			matrizJuego[2][0] = valorDeJugada;
-		} else if (posicion == 8) {
+		} else if (posicion == 7) {
 			matrizJuego[2][1] = valorDeJugada;
-		} else if (posicion == 1) {
+		} else if (posicion == 8) {
 			matrizJuego[2][2] = valorDeJugada;
 		}
-		
+
 	}
-	
+
 	public int mostrarValorMatriz(int posFila, int posColumna) {
 		return matrizJuego[posFila][posColumna];
 	}
-	
+
 	public void mostrarMatriz() {
-		
+
 		String cadena = "";
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				cadena = cadena + matrizJuego[i][j] + " -||- ";
+
+				cadena = cadena + "| " + matrizJuego[i][j] + " |";
+
+				if (j == 2) {
+					cadena = cadena + "\n";
+				}
+
 			}
 		}
-		
-		JOptionPane.showMessageDialog(null, cadena);
-		
+
+		System.out.println(cadena);
+
 	}
-	
+
+	public int jugarAtacar() {
+		int posicionPC = 0;
+		if (matrizJuego[1][0] == 1 && matrizJuego[1][1] == 1
+				&& matrizJuego[1][2] == 0) {
+			asignarValorMatriz(1, 5);
+			yaSeJugo = true;
+			posicionPC = 5;
+		}
+		return posicionPC;
+	}
+
+	public int jugarDefender() {
+		int posicionPC = 0;
+		if (matrizJuego[0][0] == 2 && matrizJuego[2][0] == 2
+				&& matrizJuego[1][0] == 0) {
+			asignarValorMatriz(1, 3);
+			yaSeJugo = true;
+			posicionPC = 3;
+		}
+		return posicionPC;
+	}
+
+	public int jugarCentro() {
+		int posicionPC = 0;
+		if (matrizJuego[1][1] == 0) {
+			asignarValorMatriz(1, 4);
+			yaSeJugo = true;
+			posicionPC = 4;
+		}
+		return posicionPC;
+	}
+
+	public int jugarPC() {
+		int posicionPC = 0;
+		if (!yaSeJugo) {
+			posicionPC = jugarAtacar();
+		}
+		if (!yaSeJugo) {
+			posicionPC = jugarDefender();
+		}
+		if (!yaSeJugo) {
+			posicionPC = jugarCentro();
+		}
+		numerojugadasPC++;
+		int i = numerojugadasPC - 1;
+		posicionJugadaPC[i] = posicionPC;
+		yaSeJugo = false;
+		return posicionPC;
+	}
 }
